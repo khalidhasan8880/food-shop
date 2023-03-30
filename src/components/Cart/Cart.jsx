8e624424774450c67c7d3498293d136b9f838ddd
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-03-30 19:22:57 +0600

    almost finished

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Cart.css'
 const Cart = ({storedMeal}) => {
+const [objMeal] = storedMeal
+
+
 
     return (
         <div>
             {
-                storedMeal.map((meal, index)=> <h1>{index + 1}. {meal.strMeal}</h1>)
+                storedMeal.map((meal, index)=> <h1 key={index}> {index + 1}. {meal.strMeal} Quantity : {meal.quantity}</h1>)
             }
+            <h3> </h3>
         </div>
     );
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Card from '../Card/Card';
 import Cart from '../Cart/Cart';
 import './Main.css'
@@ -6,27 +7,52 @@ const Main = () => {
 
     const [meals, setMeals] = useState([])
     const [storedMeal, setStoredMeal] = useState([])
-    useEffect(()=>{
-      fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-      .then(res=>res.json())
-      .then(data=> setMeals(data.meals))
-    },[])
+    useEffect(() => {
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+            .then(res => res.json())
+            .then(data => setMeals(data.meals))
+    }, [])
+
+    useEffect(() => {
+        const shoppingCart = getShoppingCart()
+        const savedMeal = []
+        console.log(shoppingCart);
+        for (const id in shoppingCart) {
+            const quantity = shoppingCart[id]
+            const finedMeal = storedMeal.find(ml => ml.idMeal === id)
+
+            if (finedMeal) {
+                finedMeal.quantity = quantity;
+                savedMeal.push(finedMeal)
+            }
+            
+        }
+        setStoredMeal(savedMeal)
+        console.log(savedMeal);
+    }, [meals])
+
 
     function buyNowHandleClick(meal) {
-        const findedMeal = storedMeal.find(ml=> ml.idMeal == meal.idMeal)
+        const findedMeal = storedMeal.find(ml => ml.idMeal == meal.idMeal)
         if (!findedMeal) {
+            meal.quantity = 1
             setStoredMeal([...storedMeal, meal])
+        }else{
+            findedMeal.quantity = findedMeal.quantity + 1;
+            const remaining = storedMeal.filter(ml=> ml.idMeal !== findedMeal.idMeal)
+            setStoredMeal([...remaining, findedMeal])
         }
-       
-
+        addToDb(meal.idMeal)
     }
+
+
 
 
     return (
         <div className='main-container'>
             <div className='card-container flex flex-wrap gap-4 p-10'>
                 {
-                    meals.map(meal=><Card key={meal.strYoutube} meal={meal} clickHandle={buyNowHandleClick}></Card>)
+                    meals.map(meal => <Card key={meal.strYoutube} meal={meal} clickHandle={buyNowHandleClick}></Card>)
                 }
             </div>
 
